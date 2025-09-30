43d65734c65d38bc6f849e900986312e8cc9ede6
eovargas-create <eovargas@conalep.edu.mx>
2025-09-30 15:59:11 +0000

    py

---
+++ DBconfiguration/docker/consultador.py
@@ -0,0 +1,27 @@
+import pyscopg2
+
+#conexión a la base de datos
+conexion = pyscopg2.connect(
+    host = "localhost",
+    port = "5432",
+    database = "credenciales",
+    user = "Admin",
+    password = "p4sw0rdDB",
+    )
+
+#Crear cursos
+cursor = conexion.cursor()
+
+# Ejecutar consola
+cursor.execute("SELECT * FROM usuarios")
+#fetchone() = una fila
+#fetchmany(n) = hasta n filas
+registros = cursor.fetchall() #Recuperar todas las filas devueltas
+#registros almacena en tuplas
+
+for fila in registros:
+    print(fila)
+
+#Cerrar la conexión
+cursor.close()
+conexion.close()
